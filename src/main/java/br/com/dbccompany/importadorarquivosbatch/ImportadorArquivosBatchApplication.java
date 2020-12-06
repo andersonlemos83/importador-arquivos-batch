0fcb13feef560debb19f33d1d963434b228fd594
alcamelo <anderson.camelo@castgroup.com.br>
2020-12-06 08:38:05 -0300

    Commit inicial

---
+++ src/main/java/br/com/dbccompany/importadorarquivosbatch/ImportadorArquivosBatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.dbccompany.importadorarquivosbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportadorArquivosBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportadorArquivosBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dbccompany/importadorarquivosbatch/ImportadorArquivosBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbccompany.importadorarquivosbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportadorArquivosBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
