521f9bd9f443125b467b7da15fd64d7ac5e7faee
alcamelo <anderson.camelo@castgroup.com.br>
2020-12-08 22:09:00 -0300

    Melhora o algoritimo para obtenção do pior vendedor.

--- src/main/java/br/com/dbccompany/importadorarquivosbatch/service/consolidador/impl/ConsolidadorPiorVendedorImpl.java
+++ src/main/java/br/com/dbccompany/importadorarquivosbatch/service/consolidador/impl/ConsolidadorPiorVendedorImpl.java
@@ -5,26 +5,49 @@ import br.com.dbccompany.importadorarquivosbatch.domain.registro.Venda;
 import br.com.dbccompany.importadorarquivosbatch.service.consolidador.ConsolidadorPiorVendedor;
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
+import java.util.stream.Collector;
 
-import static java.util.Comparator.comparingDouble;
+import static java.util.Comparator.naturalOrder;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 @Component
 public class ConsolidadorPiorVendedorImpl implements ConsolidadorPiorVendedor {
 
     @Override
     public String consolidar(List<Registro> registros) {
-        final Optional<Venda> vendaOptional = registros
-                .stream()
+        final List<Venda> vendas = filtrarVendas(registros);
+        final List<Map.Entry<String, Double>> vendedoresOrdenados = gerarListaDeVendedoresOrdenadosPeloPiorDesempenho(vendas);
+        return obterNomeDoPiorVendedor(vendedoresOrdenados);
+    }
+
+    private List<Venda> filtrarVendas(List<Registro> registros) {
+        return registros.stream()
                 .filter(Registro::ehVenda)
                 .map(registro -> (Venda) registro)
-                .sorted(comparingDouble(Venda::obterTotal))
-                .findFirst();
+                .collect(toList());
+    }
 
-        if (!vendaOptional.isPresent()) {
-            return null;
-        }
-        return vendaOptional.get().getNomeVendedor();
+    private List<Map.Entry<String, Double>> gerarListaDeVendedoresOrdenadosPeloPiorDesempenho(List<Venda> vendas) {
+        return vendas.stream()
+                .collect(gerarMapVendedoresIhTotalEmVendas())
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(naturalOrder()))
+                .collect(toList());
+    }
+
+    private Collector<Venda, ?, LinkedHashMap<String, Double>> gerarMapVendedoresIhTotalEmVendas() {
+        return toMap(Venda::getNomeVendedor, Venda::obterTotal, (totalAtual, totalNovo) -> totalAtual + totalNovo, LinkedHashMap::new);
+    }
+
+    private String obterNomeDoPiorVendedor(List<Map.Entry<String, Double>> vendedoresOrdenados) {
+        return vendedoresOrdenados.stream()
+                .findFirst()
+                .map(Map.Entry::getKey)
+                .orElse(null);
     }
 }
