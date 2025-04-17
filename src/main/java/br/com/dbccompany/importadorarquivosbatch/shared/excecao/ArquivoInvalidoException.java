fe286aff051dacbf3e0090d3402d04e4659279d3
Anderson Lemos Camelo <andersonlemos83@gmail.com>
2025-04-17 17:09:42 -0300

    Resolve débitos técnicos do sonar.

--- src/main/java/br/com/dbccompany/importadorarquivosbatch/shared/excecao/ArquivoInvalidoException.java
+++ src/main/java/br/com/dbccompany/importadorarquivosbatch/shared/excecao/ArquivoInvalidoException.java
@@ -1,20 +1,18 @@
 package br.com.dbccompany.importadorarquivosbatch.shared.excecao;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.nio.file.Path;
 
+@SuppressWarnings("java:S1165")
+@Getter
+@Setter
 public class ArquivoInvalidoException extends RuntimeException {
 
     private transient Path arquivoPath;
 
     public ArquivoInvalidoException(String mensagem) {
         super(mensagem);
-    }
-
-    public Path getArquivoPath() {
-        return arquivoPath;
-    }
-
-    public void setArquivoPath(Path arquivoPath) {
-        this.arquivoPath = arquivoPath;
     }
 }
